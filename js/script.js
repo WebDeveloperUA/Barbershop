0d661fe87dce10c275ccd7eb77d013c00aa462ae
Petro Semochko <petrosem@ukr.net>
2022-11-01 15:16:52 +0000

    set the tabulation

--- js/script.js
+++ js/script.js
@@ -1,30 +1,28 @@
-var loginLink = document.querySelector('.login-link')
-var modalLogin = document.querySelector('.modal-login')
-var modalClose = modalLogin.querySelector('.modal-close')
-var loginLogin = modalLogin.querySelector('[name=login]')
-var modalMap = document.querySelector('.modal-map')
-var mapClose = modalMap.querySelector('.modal-close')
-var mapButton = document.querySelector('.map-button')
+var loginLink = document.querySelector(".login-link");
+var modalLogin = document.querySelector(".modal-login");
+var modalClose = modalLogin.querySelector(".modal-close");
+var loginLogin = modalLogin.querySelector("[name=login]");
+var modalMap = document.querySelector(".modal-map");
+var mapClose = modalMap.querySelector(".modal-close");
+var mapButton = document.querySelector(".map-button");
 
-
-
-loginLink.addEventListener("click", function(evt){
-evt.preventDefault();
-modalLogin.classList.add("modal-show")
-loginLogin.focus();
-});
-
-modalClose.addEventListener("click", function(evt){
-evt.preventDefault();
-modalLogin.classList.remove("modal-show")
-});
-
-mapButton.addEventListener("click", function(evt){
+loginLink.addEventListener("click", function (evt) {
     evt.preventDefault();
-    modalMap.classList.add("modal-show")
-    });
+    modalLogin.classList.add("modal-show");
+    loginLogin.focus();
+});
 
-mapClose.addEventListener("click", function(evt){
-        evt.preventDefault();
-        modalMap.classList.remove("modal-show")
-        });
+modalClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalLogin.classList.remove("modal-show");
+});
+
+mapButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalMap.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalMap.classList.remove("modal-show");
+});
